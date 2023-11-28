7f886a876caf39b381d232abcd311d224f80b284
Katey Jackson <80415252+kateyjackson@users.noreply.github.com>
2023-11-28 17:21:41 -0500

    Updated Git Ignore and Readme.

---
+++ src/ava-xmcloud-starter-kit/src/temp/page-props-factory-plugins.ts
@@ -0,0 +1,5 @@
+export { componentPropsPlugin } from 'src/lib/page-props-factory/plugins/component-props';
+export { normalModePlugin } from 'src/lib/page-props-factory/plugins/normal-mode';
+export { personalizePlugin } from 'src/lib/page-props-factory/plugins/personalize';
+export { previewModePlugin } from 'src/lib/page-props-factory/plugins/preview-mode';
+export { sitePlugin } from 'src/lib/page-props-factory/plugins/site';
